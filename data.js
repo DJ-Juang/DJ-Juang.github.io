877f9da73fb48f814b409e0b8a5d2523b63e9571
DJ-Juang <derjengjuang@gmail.com>
2026-05-18 10:43:42 +0800

    Update data.js

--- data.js
+++ data.js
@@ -2,6 +2,6 @@
 const rawData = [
     {id: 1, year: "2024", region: "中國", country: "上海", url: "https://drive.google.com/file/d/1m_UFVphK9yd-FiRcfWvChMOm0G8tMnOK/view", memo1: "(外灘)"},
     {id: 2, year: "2024", region: "中國", country: "上海", url: "https://drive.google.com/file/d/1mOvsoJjDDRhVSEfAoHo3EX8viqonhIUn/view", memo1: ""},
+    {id: 202, year: "2025", region: "西歐", country: "荷蘭",url: "https://drive.google.com/file/d/1AOVyoWEGzPf4SwMEtJEAufxEthdqp6jg/view"},
     // ... 請在這裡貼上你所有的資料 ...
-    {id: 202, year: "2025", region: "西歐", country: "荷蘭", url: "https://drive.google.com/file/d/...", memo1: ""} 
 ];
